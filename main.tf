9b8cbd16cd70a4da8d58034d218085e9cc53da03
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-12-07 17:02:34 +0000

    use variable object for limitrange

--- main.tf
+++ main.tf
@@ -96,17 +96,18 @@ resource "kubernetes_limit_range" "default" {
   metadata {
     name = "${var.app_name}"
   }
-  spec {
-    limit {
-      type = "Container"
-      default = {
-        memory = "${var.container_memory_limit}"
-        cpu = "${var.container_cpu_limit}"
-      }
-      default_request = {
-        memory = "${var.container_memory_request}"
-        cpu = "${var.container_cpu_request}"
-      }
-    }
-  }
+  ${var.limitrange_default}
+#  spec {
+#    limit {
+#      type = "Container"
+#      default = {
+#        memory = "${var.container_memory_limit}"
+#        cpu = "${var.container_cpu_limit}"
+#      }
+#      default_request = {
+#        memory = "${var.container_memory_request}"
+#        cpu = "${var.container_cpu_request}"
+#      }
+#    }
+#  }
 }
