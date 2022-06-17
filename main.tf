e281e24a0c3e728c1600ff20941d014b01d1d144
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2022-06-17 14:01:22 +0000

    remove Interpolation-only expressions

--- main.tf
+++ main.tf
@@ -1,65 +1,65 @@
 provider "kubernetes" {
   load_config_file = false
-  host = "${var.host}"
-  username = "${var.username}"
-  password = "${var.password}"
-  client_certificate = "${var.client_certificate}"
-  client_key = "${var.client_key}"
-  cluster_ca_certificate = "${var.cluster_ca_certificate}"
+  host = var.host
+  username = var.username
+  password = var.password
+  client_certificate = var.client_certificate
+  client_key = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
 }
 
 resource "kubernetes_deployment" "default" {
   
   metadata {
-    name = "${var.app_name}"
+    name = var.app_name
     labels = {
-      app = "${var.app_name}"
+      app = var.app_name
     }
   }
 
   spec {
-    replicas = "${var.replicas}"
+    replicas = var.replicas
   
     selector {
       match_labels = {
-        app = "${var.app_name}"
+        app = var.app_name
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "${var.app_name}"
+          app = var.app_name
         }
       }
 
       spec {
         image_pull_secrets { 
-          name = "${var.image_pull_secrets}"
+          name = var.image_pull_secrets
         }
 
         container {
-          image = "${var.docker_image}"
-          name = "${var.app_name}"
+          image = var.docker_image
+          name = var.app_name
 
           env_from {
             secret_ref {
-              name = "${var.env_from}"
+              name = var.env_from
             }
           }
           
           volume_mount {
-            name = "${var.app_name}"
-            mount_path = "${var.mount_path}"
+            name = var.app_name
+            mount_path = var.mount_path
           }
 
-          command = "${var.command}"
+          command = var.command
         }
 
         volume {
-          name = "${var.app_name}"
+          name = var.app_name
           persistent_volume_claim {
-          claim_name = "${var.pvc_claim_name}"
+          claim_name = var.pvc_claim_name
           }
         }
       }
@@ -69,43 +69,43 @@ resource "kubernetes_deployment" "default" {
 
 resource "kubernetes_service" "default" {
   metadata {
-    name = "${var.app_name}"
+    name = var.app_name
     labels = {
-      app = "${var.app_name}"
+      app = var.app_name
     }
   }
   spec {
     selector = {
-      app = "${kubernetes_deployment.default.metadata.0.labels.app}"
+      app = kubernetes_deployment.default.metadata.0.labels.app
     }
     session_affinity = "ClientIP"
     
     port {
-      port        = "${var.port}"
-      target_port = "${var.port}"
+      port        = var.port
+      target_port = var.port
     }
 
-    load_balancer_ip = "${var.load_balancer_ip}"
-    load_balancer_source_ranges = "${var.load_balancer_source_ranges}"
+    load_balancer_ip = var.load_balancer_ip
+    load_balancer_source_ranges = var.load_balancer_source_ranges
 
-    type = "${var.service_type}"
+    type = var.service_type
   }
 }
 
 resource "kubernetes_limit_range" "default" {
   metadata {
-    name = "${var.app_name}"
+    name = var.app_name
   }
   spec {
     limit {
       type = "Container"
       default = {
-        memory = "${var.container_memory_limit}"
-        cpu = "${var.container_cpu_limit}"
+        memory = var.container_memory_limit
+        cpu = var.container_cpu_limit
       }
       default_request = {
-        memory = "${var.container_memory_request}"
-        cpu = "${var.container_cpu_request}"
+        memory = var.container_memory_request
+        cpu = var.container_cpu_request
       }
     }
   }
